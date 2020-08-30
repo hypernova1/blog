4faa83ed0d528fee89621d895c4df2881f0e166b
hypernova1 <chtlstjd01@gmail.com>
2020-08-30 21:40:29 +0900

    DateAudit 작성, DB 초기 설정

---
+++ src/test/java/org/blog/api/ApplicationTests.java
@@ -0,0 +1,23 @@
+package org.blog.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by melchor
+ * Date: 2020/08/30
+ * Time: 9:23 PM
+ */
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void test() {
+        System.out.println(1);
+    }
+
+
+
+}
