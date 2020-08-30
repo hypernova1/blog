a66e9b7351640c971e5bf5448c9a9f9f405022ad
hypernova1 <chtlstjd01@gmail.com>
2020-08-30 22:21:13 +0900

    스웨거 글로벌 설정 추가

--- src/main/java/org/blog/api/config/SwaggerConfig.java
+++ src/main/java/org/blog/api/config/SwaggerConfig.java
@@ -2,16 +2,20 @@ package org.blog.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by melchor
@@ -28,7 +32,14 @@ public class SwaggerConfig {
     @Bean
     public Docket aptV1() {
         version = "V1";
-        title = "Blog API" + version;
+        title = "Blog API " + version;
+
+        List<ResponseMessage> responseMessages = new ArrayList<>();
+        responseMessages.add(new ResponseMessageBuilder().code(200).message("OK").build());
+        responseMessages.add(new ResponseMessageBuilder().code(201).message("Created").build());
+        responseMessages.add(new ResponseMessageBuilder().code(400).message("Bad Request").build());
+        responseMessages.add(new ResponseMessageBuilder().code(401).message("Unauthorized").build());
+        responseMessages.add(new ResponseMessageBuilder().code(404).message("Not Found").build());
 
         return new Docket(DocumentationType.SWAGGER_2)
                 .useDefaultResponseMessages(false)
@@ -37,13 +48,14 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("org.blog.api.controller"))
                 .paths(PathSelectors.ant("/v1/api/**"))
                 .build()
-                .apiInfo(apiInfo(title, version));
+                .apiInfo(apiInfo(title, version))
+                .globalResponseMessage(RequestMethod.GET, responseMessages);
     }
 
     @Bean
     public Docket apiV2() {
         version = "V2";
-        title = "Blog API" + version;
+        title = "Blog API " + version;
 
         return new Docket(DocumentationType.SWAGGER_2)
                 .useDefaultResponseMessages(false)
