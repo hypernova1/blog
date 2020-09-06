24582198814a371290d3882687da42535adc9b12
hypernova1 <chtlstjd01@gmail.com>
2020-09-06 20:59:52 +0900

    ssl 활성화

--- src/main/java/org/blog/api/Application.java
+++ src/main/java/org/blog/api/Application.java
@@ -1,10 +1,13 @@
 package org.blog.api;
 
-import org.blog.api.domain.Account;
-import org.blog.api.repository.AccountRepository;
-import org.springframework.boot.CommandLineRunner;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -16,17 +19,31 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner commandLineRunner(AccountRepository accountRepository) {
-//        return args -> {
-//            Account account = Account.builder()
-//                    .email("hypemova@gmail.com")
-//                    .username("sam")
-//                    .password("1111")
-//                    .build();
-//
-//            accountRepository.save(account);
-//        };
-//    }
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
 
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+    }
+
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
 }
